3210148d8df748acea3bf193771ef623a490e688
virghiu <virghiu@gmail.com>
2023-10-05 18:06:07 -0400

    feat: render form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState } from 'react'
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
-};
-export default App;
+  const [count, setCount] = useState(1)
+
+  const handleChange = () => {
+    if (count > 8) return
+
+    setCount(count + 1)
+  }
+
+  return (
+    <section>
+      <h2>Tired of boring lorem ipsum?</h2>
+      <form>
+        <input
+          type='text'
+          id='count'
+          name='count'
+          value={count}
+          onChange={handleChange}
+        />
+        <button type='submit'>Generate</button>
+      </form>
+    </section>
+  )
+}
+export default App
